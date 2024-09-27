7768c144fa1a70b8ae0cc1b78021a6142e4435eb
LunethIV <149028919+LunethIV@users.noreply.github.com>
2024-09-27 20:52:07 +0200

    Add files via upload

---
+++ script_ej4.js
@@ -0,0 +1,57 @@
+function generarCarta(){
+    let carta = Math.floor(Math.random() * 11) + 1;
+    return carta;
+}
+
+function generarCartaCrupier(){
+    let carta = Math.floor(Math.random() * 8) + 14;
+    return carta;
+}
+
+function blackjack(){
+    
+    let puntuacionJugador = generarCarta();
+
+    const puntos = document.getElementById("puntos");
+    puntos.textContent = puntuacionJugador;
+
+    const preguntaOtra = document.getElementById("preguntaOtra");
+    preguntaOtra.textContent = "¿Quieres otra carta?";
+
+    const pedirOtra = document.getElementById("pedirOtra");
+    pedirOtra.addEventListener("click", function(){
+        if(puntuacionJugador < 21){
+            puntos.textContent = puntuacionJugador += generarCarta();
+        }else{
+            resultadoJugador.textContent = "Jugador: " + puntuacionJugador;
+            mensaje.textContent = "Has perdido!";  
+        }
+    });
+
+    const plantarse = document.getElementById("plantarse");
+    plantarse.addEventListener("click", function(){
+        let puntuacionCrupier;
+        const resultadoJugador = document.getElementById("resultadoJugador");
+        const resultadoCasa = document.getElementById("resultadoCasa");
+        const mensaje = document.getElementById("mensaje");
+
+        if(puntuacionJugador <= 21){
+            puntuacionCrupier = generarCartaCrupier();
+        }
+
+        if(puntuacionJugador <= 21 && puntuacionJugador > puntuacionCrupier){
+            resultadoJugador.textContent = "Jugador: " + puntuacionJugador;
+            resultadoCasa.textContent = "Casa: " + puntuacionCrupier;
+            mensaje.textContent = "¡Has ganado!";
+        }else if(puntuacionCrupier <= 21 && puntuacionCrupier > puntuacionJugador){
+            resultadoJugador.textContent = "Jugador: " + puntuacionJugador;
+            resultadoCasa.textContent = "Casa: " + puntuacionCrupier;
+            mensaje.textContent = "¡Has perdido!";        
+        }else if(puntuacionJugador > 21){
+            resultadoJugador.textContent = "Jugador: " + puntuacionJugador;
+            resultadoCasa.textContent = "Casa: " + puntuacionCrupier;
+            mensaje.textContent = "¡Has perdido!";       
+        }
+    })
+}
+blackjack();
